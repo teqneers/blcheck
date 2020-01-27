aead7792e855a8ad441898e578f992a9983e8d21
Sven Walloner <sw.coding@mailbox.org>
2020-01-27 16:27:02 +0100

    first draft for golang cli command (#1)
    * first initial go draft
    
    * refactor to viper, async call adjustments, use color output
    
    * changed progressbar lib, some more refinement
    
    * added binary
    
    * removed binary
    
    * added binary
    
    * updated binary

---
+++ iputil/iputil_test.go
@@ -0,0 +1,41 @@
+package iputil
+
+import (
+	"testing"
+)
+
+func TestReverseIP(t *testing.T) {
+	testIP := "1.2.3.4"
+	reverseIP := Reverse(testIP)
+
+	if reverseIP != "4.3.2.1" {
+		t.Errorf("Reversing ip failed: got %v want %v", reverseIP, "4.3.2.1")
+	}
+}
+
+func TestValidateIP(t *testing.T) {
+	testIP := "1.2.3.4"
+	isValid := Validate(testIP)
+
+	if isValid == false {
+		t.Errorf("Validating IP failed, IP is invalid but should be valid")
+	}
+}
+
+func TestValidateIPButInvalid(t *testing.T) {
+	testIP := "300.2.3.4"
+	isValid := Validate(testIP)
+
+	if isValid == true {
+		t.Errorf("Validating IP failed, IP is valid but should be invalid")
+	}
+}
+
+func TestValidateIPButTooLong(t *testing.T) {
+	testIP := "1.2.3.4.5"
+	isValid := Validate(testIP)
+
+	if isValid == true {
+		t.Errorf("Validating IP failed, IP is valid but should be invalid")
+	}
+}
